80c5e8e0ea9bc63c990466772d0b43ffb10a058d
Anita Prajapati <anita.prajapati@cumminscollege.in>
2026-04-15 12:41:04 +0530

    Added jenkins file:

--- src/test/java/com/example/LoginValidatorTest.java
+++ src/test/java/com/example/LoginValidatorTest.java
@@ -1,5 +1,5 @@
 package com.example;
-import com.example.LoginValidator;
+//import com.example.LoginValidator;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
